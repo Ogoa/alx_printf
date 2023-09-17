5b7fa17175d36b3cff4140e135fd38cc9b0cc9ac
Ogoa <ogopabhs@gmail.com>
2023-09-17 13:28:43 +0000

    Attempt task 0 of printf
    I created a _putchar.c file that has the utility _putchar() function.
    my_printf.c has the _printf() function, where in line with task 0, I
    handled the conversion specifiers: 'c', 's' and '%'.
    I also created a main.h header file that contains all function
    prototypes.

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _print_str(const char *);
+
+#endif /* #ifndef MAIN_H */
---
+++ my_printf.c
@@ -0,0 +1,79 @@
+#include <stddef.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format
+ * @format: Pointer to the memory location holding the string
+ * that acts as a format specifier of what is to be printed
+ *
+ * Return: The number of characters printed (excluding the null
+ * byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int printed_chars = 0;
+	char c; /* Will temporarily hold a character retrieved from the list */
+	char *str; /* Will temporarily hold a string retrieved from the list */
+
+	if (format == NULL)
+		return (0);
+	va_start(args, format);
+	while (*(format + i) != '\0')
+	{
+		if (*(format + i) != '%')
+		{
+			_putchar(*(format + i));
+			printed_chars++;
+		}
+		else if (*(format + i) == '%')
+		{
+			i++;
+			switch (*(format + i))
+			{
+			case 'c':
+				c = va_arg(args, int);
+				_putchar(c);
+				printed_chars++;
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				printed_chars += _print_str(str);
+				break;
+			case '%':
+				_putchar('%');
+				printed_chars++;
+				break;
+			default:
+				break;
+			}
+		}
+		i++;
+	}
+	va_end(args);
+	return (printed_chars);
+}
+
+/**
+ * _print_str - Prints a string value
+ * @str: Pointer to the memory location holding the null-terminated
+ * array of characters
+ *
+ * Return: The length of the string (printed characters)
+ * 0 if the 'str' has a NULL reference
+ */
+int _print_str(const char *str)
+{
+	int str_length = 0;
+
+	if (str == NULL)
+		return (0);
+	while (*(str + str_length) != '\0')
+	{
+		_putchar(*(str + str_length));
+		str_length++;
+	}
+	return (str_length);
+}
