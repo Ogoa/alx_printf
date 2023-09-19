b5e9f93cf0ff592bb0a74698a78dd18c9bbe5980
Hansel Omondi <87500038+Ogoa@users.noreply.github.com>
2023-09-20 01:48:13 +0300

    Add is_digit function to width.c

--- width.c
+++ width.c
@@ -36,3 +36,16 @@ int get_width(const char *format, int *i, va_list args)
 
 	return (width);
 }
+
+/**
+ * is_digit -Verifies if char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit otherwise 0
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
