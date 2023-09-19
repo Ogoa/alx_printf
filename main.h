98666eab9a7522426d81efefaaf4a145cca43597
Steve Martine <stevemartinez979@gmail.com>
2023-09-19 11:05:51 -0700

    size

--- main.h
+++ main.h
@@ -7,6 +7,17 @@
 #define BUFF_SIZE 1024
 #define UNUSED(x) (void)(x)
 
+/*FLAGS*/
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/*SIZE*/
+#define S_LONG 2
+#define S_SHORT 1
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _print_str(const char *);
